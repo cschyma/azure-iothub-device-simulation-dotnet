7625c811243e633b2fe829ff5563c9422db0f34f
Ranga Vadlamudi <rangv@microsoft.com>
2017-01-10 21:35:33 -0600

    initial code
    initial code

---
+++ AzureIoTHub.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+static class AzureIoTHub
+{
+    //
+    // Note: this connection string is specific to the device "myDotnetDevice". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName={hub}.azure-devices.net;DeviceId=myDotnetDevice;SharedAccessKey={SAS}";
+
+    //
+    // To monitor messages sent to device "myDotnetDevice" use iothub-explorer as follows:
+    //    iothub-explorer HostName=rviothub12052016.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=dNRzycunBnj3JS4OJ4ZKTG7p6g5X2rX6JwEVdprWZUg= monitor-events "myDotnetDevice"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+#if WINDOWS_UWP
+        var str = "Hello, Cloud from a UWP C# app!";
+#else
+        var str = "Hello, Cloud from a C# app!";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+
+namespace azure_iothub_device_simulation_dotnet
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "HostName={hub}.azure-devices.net;DeviceId=myDotnetDevice;SharedAccessKey={SAS}";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.CreateFromConnectionString(iotHubUri, TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+
+        }
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double temparature = 70; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currenttemparature = temparature + rand.NextDouble() * 10;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myDotnetDevice",
+                    temparature = currenttemparature
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
